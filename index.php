cf742f6779639bfb8d29605c40532ec0a06e02ee
Ezzaouya_Ilyass <156593840+Techinho@users.noreply.github.com>
2024-11-03 13:55:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// Database connection parameters.
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'todolist');
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '3306');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  
+</body>
+</html>
